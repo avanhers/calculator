05c10c9e397d94bc2cbd81bbec2baf4dc53109aa
Antoine VANHERSECKE <antoine.vanhersecke@ubiwan.local>
2023-09-07 15:08:39 +0200

    feat(reader): rework reader, use event 'line' of readline

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 const parser = require("./parser");
 const solver = require("./solver")
-const reader = require("./reader")
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 
 async function run() {
-
-    while (1) {
+    console.log("Write an operation")
+    readline.on('line', (input) => {
         try {
-            let input = await reader.readInput()
             let calculationArray = parser.parseInput(input)
             let result = solver.solve(calculationArray, true, true)
             console.log(result)
         } catch (e) {
             console.log('Something went wrong: ', e)
         }
-    }
+      }); 
 }
 
 run()
+
+module.exports = { run }
