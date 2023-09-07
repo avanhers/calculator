fc1bf63288f37c712cf8ea9c7c2d20fb23b53341
Antoine VANHERSECKE <antoine.vanhersecke@ubiwan.local>
2023-09-07 11:03:43 +0200

    feat(solver): remove Operation class

--- solver.js
+++ solver.js
@@ -1,51 +1,35 @@
-// Class supporting a basic Operation 
-class Operation {
-    lValue;
-    rValue;
-    operator;
-
-    constructor(arr) {
-        if (arr.length != 3) {
-            throw 'Invalid Operation'
-        }
-        this.lValue = arr[0];
-        this.operator = arr[1];
-        this.rValue = arr[2]
-    }
-
-    evaluate() {
-        switch (this.operator) {
-            case "+":
-                return this.lValue + this.rValue
-            case "-":
-                return this.lValue - this.rValue
-            case "/":
-                if (this.rValue === 0) {
-                     throw `invalid operation: division by zero is not possible in : ${this.lValue}${this.operator}${this.rValue}`
-                }
-                return this.lValue / this.rValue
-            case "*":
-                return this.lValue * this.rValue
-            default:
-                throw 'invalid operation: unkown operand '
-        }
-    }
-}
-
-
+// function that evaluate a single operation 
+// should take an array of the form [float, 'operator',float]
 function processOperation(operationArray) {
-    let operation = new Operation(operationArray)
-    return operation.evaluate()
+    if (operationArray.length != 3) {
+        throw 'Invalid Operation'
+    }
+    switch (operationArray[1]) {
+        case "+":
+            return operationArray[0] + operationArray[2]
+        case "-":
+            return operationArray[0] - operationArray[2]
+        case "/":
+            if (this.rValue === 0) {
+                throw `invalid operation: division by zero is not possible in : ${operationArray[0]}${operationArray[1]}${operationArray[2]}`
+            }
+            return operationArray[0] / operationArray[2]
+        case "*":
+            return operationArray[0] * operationArray[2]
+        default:
+            throw 'invalid operation: unkown operand '
+    }
 }
+
 
 // Recursive function that solve a calculation array
 // argument checkParentheses and checkPrios are boolean, 
 // thos arguments are here to avoid unecessary operation after a recursive call
 function resolve(array, checkParentheses, checkPrios) {
     let len = array.length
-    if (len === 3 ) {
+    if (len === 3) {
         return processOperation(array)
-    } else if (len === 1){
+    } else if (len === 1) {
         return array[0]
     }
 
@@ -53,7 +37,7 @@ function resolve(array, checkParentheses, checkPrios) {
         for (let index = 0; index < len; index++) {
             if (array[index] === '(') {
                 updatedArray = handleParentheses(array, index)
-                
+
                 return resolve(updatedArray, true, true)
             }
         }
@@ -62,7 +46,7 @@ function resolve(array, checkParentheses, checkPrios) {
     if (checkPrios) {
         for (let index = 0; index < len; index++) {
             if (array[index] === '*' || array[index] === '/') {
-                updatedArray = doPriorityOperation(array, index)  
+                updatedArray = doPriorityOperation(array, index)
                 return resolve(updatedArray, false, true)
             }
         }
@@ -72,11 +56,11 @@ function resolve(array, checkParentheses, checkPrios) {
 
 // function resolving a bracketOperation
 function handleParentheses(array, index) {
-    let closingBracketIndex = findMatchingBracket(array,index,'(',')')
+    let closingBracketIndex = findMatchingBracket(array, index, '(', ')')
     subArr = array.slice(index + 1, closingBracketIndex)
-    let bracketResult =  resolve(subArr, true, true)
+    let bracketResult = resolve(subArr, true, true)
     let startingArray = index - 1 < 0 ? [] : array.slice(0, index);
-    let endingArray = array.slice(closingBracketIndex+1);
+    let endingArray = array.slice(closingBracketIndex + 1);
     newArr = startingArray.concat(bracketResult).concat(endingArray);
     return newArr
 }
@@ -85,7 +69,7 @@ function handleParentheses(array, index) {
 function doPriorityOperation(array, index) {
     let startingArray = index - 1 <= 0 ? [] : array.slice(0, index - 1)
     let endingArray = array.slice(index + 2)
-    
+
     let operationArr = array.slice(index - 1, index + 2)
     blockResult = processOperation(operationArr)
     newArr = startingArray.concat(blockResult).concat(endingArray);
@@ -101,26 +85,26 @@ function doRest(array) {
 
 
 // given index of an opening bracket find the index of corresponding closing bracket.
-function findMatchingBracket(str, index, openingBraket, closingBraket){
+function findMatchingBracket(str, index, openingBraket, closingBraket) {
     let find = 1
     let len = str.length
-    while (index < len && find !=0 ){
+    while (index < len && find != 0) {
         index++;
-       switch (str[index]){
-        case openingBraket:
-            find++
-            break;
-        case closingBraket:
-            find--
-            break;
-        default:
-            break;
-       }
+        switch (str[index]) {
+            case openingBraket:
+                find++
+                break;
+            case closingBraket:
+                find--
+                break;
+            default:
+                break;
+        }
     }
-    if (index === len && find !=0 ){
+    if (index === len && find != 0) {
         throw 'Invalid Operation, at least one closing bracket is missing'
     }
     return index;
 }
 
-module.exports = { resolve}
+module.exports = { resolve }
